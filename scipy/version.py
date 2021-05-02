f25031ec6421aaab24ec2008d9ceef15428ca8c9
Ralf Gommers <ralf.gommers@gmail.com>
2021-05-02 16:41:07 +0200

    WIP: commit generated __config__.py and version.py files
    This should be dynamically generated later, but for now we need
    these to be able to import scipy and run the tests.

---
+++ scipy/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.7.0'
+version = '1.7.0'
+full_version = '1.7.0.dev0+869.30db361'
+git_revision = '30db361'
+commit_count = '869'
+release = False
+
+if not release:
+    version = full_version
