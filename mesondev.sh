48af5910f10395a4add1b71ab60041dbe2e57f71
Ralf Gommers <ralf.gommers@gmail.com>
2021-05-22 16:01:02 +0200

    BLD: add Meson support to fftpack submodule
    Also a few tweaks to mesondev.sh

--- mesondev.sh
+++ mesondev.sh
@@ -1,3 +1,6 @@
+# Stop on error
+set -e
+
 # Build SciPy - use a prefix, without it is broken
 meson setup build --prefix=/home/rgommers/code/bldscipy/installdir
 ninja -C build
@@ -6,5 +9,12 @@ meson install
 
 export PYTHONPATH=~/code/bldscipy/installdir/lib/python3.9/site-packages/
 python -c "from scipy import odr as s; s.test()"
-python -c "from scipy import _lib as s; s.test()"
+python -c "from scipy import fftpack as s; s.test()"
+python -c "from scipy import fft as s; s.test()"
+#python -c "from scipy import _lib as s; s.test()"
 popd
+
+# Notes:
+#
+# In case of an issue with build options, use the introspect command:
+#   meson introspect build/ -i --targets
