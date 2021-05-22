b3571c4e86c4cc1e54ff8fa43d48b50e699ba301
Ralf Gommers <ralf.gommers@gmail.com>
2021-05-22 16:59:36 +0200

    BLD: add Meson support for constants submodule

--- mesondev.sh
+++ mesondev.sh
@@ -8,10 +8,12 @@ pushd build
 meson install
 
 export PYTHONPATH=~/code/bldscipy/installdir/lib/python3.9/site-packages/
+python -c "from scipy import constants as s; s.test()"
+# python -c "from scipy import ndimage as s; s.test()"  # relies on special
 python -c "from scipy import odr as s; s.test()"
 python -c "from scipy import fftpack as s; s.test()"
 python -c "from scipy import fft as s; s.test()"
-#python -c "from scipy import _lib as s; s.test()"
+#python -c "from scipy import _lib as s; s.test()"  # relies on spatial
 popd
 
 # Notes:
