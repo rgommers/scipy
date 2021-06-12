1965f175493a21f5dcc5e1d9d8fd6e4f33ca5267
Ralf Gommers <ralf.gommers@gmail.com>
2021-06-13 00:47:13 +0200

    DEV: comment out the tests for modules that depend on other modules
    All tests that are run now pass again

--- mesondev.sh
+++ mesondev.sh
@@ -11,12 +11,12 @@ popd
 pushd installdir
 
 export PYTHONPATH=~/code/bldscipy/installdir/lib/python3.9/site-packages/
-python -c "from scipy import linalg as s; s.test()"
+# python -c "from scipy import linalg as s; s.test()"  # relies on sparse
 python -c "from scipy import constants as s; s.test()"
 # python -c "from scipy import ndimage as s; s.test()"  # relies on special
 python -c "from scipy import odr as s; s.test()"
-python -c "from scipy import fftpack as s; s.test()"
-python -c "from scipy import fft as s; s.test()"
+# python -c "from scipy import fftpack as s; s.test()"  # relies on fft
+#python -c "from scipy import fft as s; s.test()"  # relies on special
 #python -c "from scipy import _lib as s; s.test()"  # relies on spatial
 popd
 
