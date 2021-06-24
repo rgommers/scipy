0a60c8c49d513adc5a8fd7ef22c203dab46a81de
Gagandeep Singh <gdp.1807@gmail.com>
2021-06-24 12:42:10 +0530

    mesondev.sh accepts build and install paths (#24)

--- mesondev.sh
+++ mesondev.sh
@@ -1,16 +1,25 @@
 # Stop on error
 set -e
 
+# Command Line Arguments
+# $1 - First argument is an absolute/relative 
+#      path to build directory.
+# $2 - Second argument is an absolute path to
+#      install directory.
+
+# TODO: Default $1 and $2 arguments to standard paths
+# once migration to meson build system is complete.
+
 # Build SciPy - use a prefix, without it is broken
-meson setup build --prefix=/home/rgommers/code/bldscipy/installdir
-ninja -C build
-pushd build
+meson setup $1 --prefix=$2
+ninja -C $1
+pushd $1
 meson install
 popd
 # Avoid being in a directory which has a scipy/ dir
-pushd installdir
+pushd $2
 
-export PYTHONPATH=~/code/bldscipy/installdir/lib/python3.9/site-packages/
+export PYTHONPATH=$2/lib/python3.9/site-packages/
 # python -c "from scipy import linalg as s; s.test()"  # relies on sparse
 python -c "from scipy import constants as s; s.test()"
 # python -c "from scipy import ndimage as s; s.test()"  # relies on special
@@ -20,6 +29,7 @@ python -c "from scipy.sparse import csgraph as s; s.test()"
 # python -c "from scipy import fftpack as s; s.test()"  # relies on fft
 # python -c "from scipy import fft as s; s.test()"  # relies on special
 # python -c "from scipy import _lib as s; s.test()"  # relies on spatial
+# python -c "from scipy import optimize as s; s.test()" # relies on sparse.linalg
 popd
 
 # Notes:
