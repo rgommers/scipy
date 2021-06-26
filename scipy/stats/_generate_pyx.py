a05fd966896a1ce95fd0e8fdba9655d546f6e139
Ralf Gommers <ralf.gommers@gmail.com>
2021-06-27 00:32:38 +0200

    BLD: fix boost Cython compilation with Meson
    This gets rid of `run_command`, and replaces it with use of
    `custom_target`. This means things now run at build-time rather
    than at configure-time, and cache will work correctly.
    
    Closes https://github.com/rgommers/scipy/issues/25

--- scipy/stats/_generate_pyx.py
+++ scipy/stats/_generate_pyx.py
@@ -2,6 +2,8 @@ import pathlib
 from shutil import copyfile
 import subprocess
 import sys
+import os
+import argparse
 
 
 def isNPY_OLD():
@@ -14,21 +16,34 @@ def isNPY_OLD():
     return ver < (1, 19)
 
 
-def make_biasedurn():
+def make_biasedurn(outdir):
     '''Substitute True/False values for NPY_OLD Cython build variable.'''
     biasedurn_base = (pathlib.Path(__file__).parent / 'biasedurn').absolute()
     with open(biasedurn_base.with_suffix('.pyx.templ'), 'r') as src:
         contents = src.read()
-    with open(biasedurn_base.with_suffix('.pyx'), 'w') as dest:
+
+    outfile = outdir / 'biasedurn.pyx'
+    with open(outfile, 'w') as dest:
         dest.write(contents.format(NPY_OLD=str(bool(isNPY_OLD()))))
 
 
-def make_boost():
+def make_boost(outdir):
     # Call code generator inside _boost directory
     code_gen = pathlib.Path(__file__).parent / '_boost/include/code_gen.py'
-    subprocess.run([sys.executable, str(code_gen)], check=True)
+    subprocess.run([sys.executable, str(code_gen), '-o', outdir],
+                   check=True)
 
 
 if __name__ == '__main__':
-    make_biasedurn()
-    make_boost()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--outdir", type=str,
+                        help="Path to the output directory")
+    args = parser.parse_args()
+
+    if not args.outdir:
+        raise ValueError(f"Missing `--outdir` argument to _generate_pyx.py")
+
+    outdir_abs = pathlib.Path(os.getcwd()) / args.outdir
+
+    make_biasedurn(outdir_abs)
+    make_boost(outdir_abs)
