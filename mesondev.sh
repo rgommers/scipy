d973aa031b4c734b32f7e3482d681a51c81134cb
czgdp1807 <gdp.1807@gmail.com>
2021-07-02 11:51:16 +0530

    optimize depends on givens_elimination

--- mesondev.sh
+++ mesondev.sh
@@ -27,16 +27,15 @@ python -c "from scipy import constants as s; s.test()"
 # python -c "from scipy import ndimage as s; s.test()"  # relies on special
 python -c "from scipy import odr as s; s.test()"
 python -c "from scipy.sparse import csgraph as s; s.test()"
-# python -c "from scipy import sparse as s; s.test()"  # relies on sparse.linalg
+# python -c "from scipy import sparse as s; s.test()"  # working
 # python -c "from scipy import fftpack as s; s.test()"  # relies on fft
 # python -c "from scipy import fft as s; s.test()"  # relies on special
 # python -c "from scipy import _lib as s; s.test()"  # relies on spatial
 # python -c "from scipy import integrate as s; s.test()" # relies on special
-# python -c "from scipy import optimize as s; s.test()" # relies on sparse.linalg
+# python -c "from scipy import optimize as s; s.test()" # relies on 'scipy.optimize._lsq.givens_elimination'
 # python -c "from scipy import stats as s; s.test()" # relies on spatial
 # python -c "from scipy import special as s; s.test()" # relies on spatial
 # python -c "from scipy import spatial as s; s.test()" # relies on sparse.linalg
-python -c "from scipy.sparse import linalg as s; s.test()"
 popd
 
 # Notes:
