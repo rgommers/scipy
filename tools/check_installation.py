4c49ea43e9e879d8ff2976d7698944165d61f142
Ralf Gommers <ralf.gommers@gmail.com>
2021-10-08 21:07:33 +0200

    WIP: update CI script to check installed files

--- tools/check_installation.py
+++ tools/check_installation.py
@@ -4,14 +4,20 @@ Script for checking if all the test files are installed after building.
 Examples::
 
     $ python check_installation.py install_directory_name
-        install_directory_name: It the name of the directory where scipy is
-                                installed after building.
 
-Note
-====
+        install_directory_name:
+            the relative path to the directory where SciPy is installed after
+            building and running `meson install`.
+
+Notes
+=====
 
 This script assumes that ``scipy`` and ``install_directory`` are present under
 the same parent folder.
+
+The script will stop on encountering the first missing file in the install dir,
+it will not give a full listing. This should be okay, because the script is
+meant for use in CI so it's not like many files will be missing at once.
 
 """
 
@@ -19,9 +25,11 @@ import os
 import glob
 import sys
 
+
 CUR_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 ROOT_DIR = os.path.dirname(CUR_DIR)
 SCIPY_DIR = os.path.join(ROOT_DIR, 'scipy')
+
 
 # Files whose installation path will be different from original one
 changed_installed_path = {
@@ -29,24 +37,38 @@ changed_installed_path = {
         'scipy/_lib/tests/test_scipy_version.py'
 }
 
-def main(argv):
-    INSTALLED_DIR = os.path.join(ROOT_DIR, argv[0])
+
+def main(install_dir):
+    INSTALLED_DIR = os.path.join(ROOT_DIR, install_dir)
+    if not os.path.exists(INSTALLED_DIR):
+        raise ValueError(f"Provided install dir {INSTALLED_DIR} does not exist")
+
     scipy_test_files = get_test_files(SCIPY_DIR)
     installed_test_files = get_test_files(INSTALLED_DIR)
+
+    # Check test files detected in repo are installed
     for test_file in scipy_test_files.keys():
         if not test_file in installed_test_files.keys():
             raise Exception("%s is not installed" % scipy_test_files[test_file])
+
+    # FIXME: this is the reverse test, but it does not seem possible to make
+    # this fail unless a file is installed twice (which is hard to do). If I
+    # move a file somewhere else, the check above already fails. So is this
+    # needed?
     for test_file in installed_test_files.keys():
         if not test_file in scipy_test_files.keys():
             raise Exception("%s is installed at improper location" %
                 installed_test_files[test_file])
     print("----------- All the test files were installed --------------")
 
+
 def get_parent_dir(current_path, levels=1):
     current_new = current_path
     for i in range(levels + 1):
         current_new = os.path.dirname(current_new)
+
     return os.path.relpath(current_path, current_new)
+
 
 def get_test_files(dir):
     test_files = dict()
@@ -54,7 +76,10 @@ def get_test_files(dir):
         suffix_path = get_parent_dir(path, 3)
         suffix_path = changed_installed_path.get(suffix_path, suffix_path)
         test_files[suffix_path] = path
+
     return test_files
 
+
 if __name__ == '__main__':
-    main(argv=sys.argv[1:])
+    install_dir = sys.argv[1]
+    main(install_dir)
