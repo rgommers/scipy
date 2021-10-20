46c6a006481e974d3939c018a78c2d53ee50135b
Matthew Brett <matthew.brett@gmail.com>
2021-10-20 12:27:22 +0100

    Allow copying with script.

--- scipy/_build_utils/copyfiles.py
+++ scipy/_build_utils/copyfiles.py
@@ -1,20 +1,20 @@
-import os
-from shutil import copyfile
+#!python3
+""" Platform independent file copier script
+"""
+
+import shutil
 import argparse
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--infiles", nargs='+',
+    parser.add_argument("infiles", nargs='+',
                         help="Paths to the input files")
-    parser.add_argument("-o", "--outdir", type=str,
+    parser.add_argument("outdir",
                         help="Path to the output directory")
     args = parser.parse_args()
-
-    outdir_abs = os.path.join(os.getcwd(), args.outdir)
     for infile in args.infiles:
-        outfile = os.path.join(outdir_abs, infile)
-        copyfile(infile, outfile)
+        shutil.copy2(infile, args.outdir)
 
 
 if __name__ == "__main__":
