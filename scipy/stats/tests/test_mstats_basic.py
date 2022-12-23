1a10268dfc5db3ef78e176c91417cc4f3c692db3
Ralf Gommers <ralf.gommers@gmail.com>
2022-12-23 22:18:58 +0100

    TST: fix misnamed stats.mstats test that wasn't running
    A test needs to start with `test_` for it to run.
    
    [skip circle]

--- scipy/stats/tests/test_mstats_basic.py
+++ scipy/stats/tests/test_mstats_basic.py
@@ -1077,8 +1077,9 @@ class TestNormalitytests():
         attributes = ('statistic', 'pvalue')
         check_named_results(res, attributes, ma=True)
 
-    def regression_test_9033(self):
-        # x cleary non-normal but power of negtative denom needs
+    def test_kurtosistest_negdenom(self):
+        # regression test for gh-9033
+        # x is clearly non-normal, but power of negative denominator needs
         # to be handled correctly to reject normality
         counts = [128, 0, 58, 7, 0, 41, 16, 0, 0, 167]
         x = np.hstack([np.full(c, i) for i, c in enumerate(counts)])
