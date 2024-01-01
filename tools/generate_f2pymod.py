1253c33caf7f0401c470b194f44051426b4904da
Ralf Gommers <ralf.gommers@gmail.com>
2024-01-01 21:01:55 +0100

    WIP: ENH: enable ILP64 BLAS/LAPACK support again
    This was left out initially when adding support for building with
    Meson. The numpy.distutils based build supported ILP64 builds with
    OpenBLAS and its `64_` symbol suffix scheme. We now add this back
    and make it more generic, so that it also supports the `_64` scheme
    of MKL as well as the `$NEWLAPACK_ILP64` scheme from Accelerate.

--- tools/generate_f2pymod.py
+++ tools/generate_f2pymod.py
@@ -263,6 +263,8 @@ def main():
                         help="Path to the input file")
     parser.add_argument("-o", "--outdir", type=str,
                         help="Path to the output directory")
+    parser.add_argument("--f2cmap", type=str,
+                        help="Path to the f2cmap file")
     args = parser.parse_args()
 
     if not args.infile.endswith(('.pyf', '.pyf.src', '.f.src')):
@@ -283,10 +285,13 @@ def main():
 
     # Now invoke f2py to generate the C API module file
     if args.infile.endswith(('.pyf.src', '.pyf')):
-        p = subprocess.Popen([sys.executable, '-m', 'numpy.f2py', fname_pyf,
-                            '--build-dir', outdir_abs], #'--quiet'],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                            cwd=os.getcwd())
+        cmd = [sys.executable, '-m', 'numpy.f2py', fname_pyf,
+               '--build-dir', outdir_abs]
+        if args.f2cmap:
+            cmd += ['--f2cmap', args.f2cmap]
+
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, cwd=os.getcwd())
         out, err = p.communicate()
         if not (p.returncode == 0):
             raise RuntimeError(f"Writing {args.outfile} with f2py failed!\n"
