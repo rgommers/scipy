4357b9e33044cd42d9a3b3db4c1d0986b80b8b5b
Ralf Gommers <ralf.gommers@gmail.com>
2026-04-05 12:27:40 +0200

    MAINT: use idiomatic `np.isdtype` and iteration over dtypes in tests

--- scipy/linalg/_misc.py
+++ scipy/linalg/_misc.py
@@ -251,12 +251,11 @@ def bandwidth(a):
     if a.ndim < 2:
         raise ValueError('Input array must be at least 2D.')
 
-    if a.dtype.char in 'egG':
-        raise TypeError('Input array with float16/float128/complex256 '
-                        'dtype is not supported.')
+    if np.isdtype(a.dtype, (np.float16, np.longdouble, np.clongdouble)):
+        raise TypeError(f'Input array with {a.dtype} dtype is not supported.')
 
     # Now that the problematic numeric types are tested, test for numeric or bool.
-    elif not (np.isdtype(a.dtype.type, "numeric") or np.isdtype(a.dtype.type, "bool")):
+    elif not np.isdtype(a.dtype, ("numeric", "bool")):
         raise TypeError(f'Input array must have a numeric dtype, got {a.dtype}.')
 
     # Empty array bandwidth is defined to be zero.
